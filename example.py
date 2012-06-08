64bbd08463c7043a0a4dd95fa1cfba4635ab8889
Balthazar Rouberol <rouberol.b@gmail.com>
2012-06-08 13:05:47 +0200

    TesseractTraining framework + example Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

---
+++ example.py
@@ -0,0 +1,51 @@
+import argparse
+from os.path import exists
+from training_framework import TesseractTrainer
+
+
+def perform_security_checks(args):
+    """ Perform several checks on the tesseract training arguments:
+        * the tif file {tesseract_lang}.{font_name}.exp{experience_number}.tif must exist
+        * the {tessdata_path} directorty must exist
+        * the font name must not contain any spaces
+        * the font defined by {font_name} must be described in the {font_properties} file
+    If any of those tests fail, the execution will stop.
+    """
+    tifname = '.'.join([args.tesseract_lang, args.font_name, 'exp'+str(args.experience_number)]) + '.tif'
+    if not exists(tifname):
+        print "The %s file does not exist. Aborting." %(tifname)
+        sys.exit(1)
+
+    if not exists(args.tessdata_path):
+        print "The %s directory does not exist. Aborting." %(args.tessdata_path)
+        sys.exit(1)
+
+    if " " in args.font_name:
+        print "The --font-name / -F argument must not contain any spaces. Aborting."
+        sys.exit(1)
+
+    if args.font_name not in open(args.font_properties, 'r').read().split():
+        print "The font properties of %s have not been defined in %s. Aborting." %(args.font_name, args.font_properties)
+        sys.exit(1)
+
+if __name__ == '__main__':
+
+    # Parse training arguments
+    parser = argparse.ArgumentParser(description='Tesseract training arguments.')
+    parser.add_argument('--experience_number', '-e', type=int, action='store', default=0, help="The number of the training experience.")
+    parser.add_argument('--font-name', '-F', type=str, action='store', required=True, help="The name of the used font. No spaces.")
+    parser.add_argument('--font-properties', '-f', type=str, action='store' , default="./font_properties",        
+        help="The path of a file containing font properties for a list of fonts.")
+    parser.add_argument('--tessdata-path', '-p', type=str, action='store', default='/usr/local/share/tessdata/', 
+        help="The path of the tessdata/ directory on your filesystem.")
+    parser.add_argument('--tesseract-lang', '-t', type=str, action='store', required=True, help="Set the tesseract language traineddata to create.") 
+    parser.add_argument('--word_list', '-w', type=str, action='store', default=None, help="The path of a file containing a list of frequent words.")
+    args = parser.parse_args()    
+
+    perform_security_checks(args) # Check validity of args
+
+    # Training process
+    trainer = TesseractTrainer(args.experience_number, args.tesseract_lang, args.font_name, args.font_properties, args.tessdata_path, args.word_list)
+    trainer.training()
+    trainer.clean()
+    trainer.add_trained_data()
---
+++ training_framework.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+"""
+A small training framework for Tesseract 3.x, taking over the tedious manual process
+described in the Tesseract Wiki: https://code.google.com/p/tesseract-ocr/wiki/TrainingTesseract3
+
+Written by Balthazar Rouberol, <balthazar@strongsteam.com>, @BaltoRouberol
+
+
+Copyright (c) 2012, Balthazar Rouberol
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+"""
+
+import sys
+from os import system
+
+class TesseractTrainer:
+    """ Object handling the training process of tesseract """
+
+    def __init__(self, exp_number, dictionary_name, font_name, font_properties, tessdata_path, word_list):
+        self.dictionary_name = dictionary_name
+        self.font_name = font_name
+        self.prefix = '%s.%s.exp%s' %(self.dictionary_name, self.font_name, str(exp_number))
+        self.font_properties = font_properties
+        self.tessdata_path = tessdata_path
+        self.word_list = word_list
+
+    def generate_boxfile(self):
+        """ Generate a boxfile from the training image """
+        cmd = 'tesseract {prefix}.tif {prefix} batch.nochop makebox'.format(prefix = self.prefix)
+        system(cmd)
+    
+    def train_on_boxfile(self):
+        """ Run tesseract on training mode, using the generated boxfiles """
+        cmd = 'tesseract {prefix}.tif {prefix} nobatch box.train'.format(prefix = self.prefix)
+        system(cmd)
+
+    def compute_character_set(self):
+        """ Computes the character properties set: isalpha, isdigit, isupper, islower, ispunctuation
+        and encode it in the 'unicharset' data file 
+
+        examples:
+        ';' is an punctuation character. Its properties are thus represented 
+            by the binary number 10000 (10 in hexadecimal).
+        'b' is an alphabetic character and a lower case character. 
+            Its properties are thus represented by the binary number 00011 (3 in hexadecimal).
+        W' is an alphabetic character and an upper case character. Its properties are
+            thus represented by the binary number 00101 (5 in hexadecimal).
+        '7' is just a digit. Its properties are thus represented by the binary number 01000 (8 in hexadecimal).
+        '=' does is not punctuation not digit or alphabetic character. Its properties
+             are thus represented by the binary number 00000 (0 in hexadecimal). 
+        """
+        cmd = 'unicharset_extractor %s.box ' %(self.prefix)
+        system(cmd)
+
+    def clustering(self):
+        """ Cluster character features from all the training pages, and create characters prototype """
+        cmd = 'mftraining -F font_properties -U unicharset %s.tr ' %(self.prefix)
+        system(cmd)
+
+    def normalize(self):
+        """ Generate the 'normproto' data file (the character normalization sensitivity prototypes) """
+        cmd = 'cntraining %s.tr ' %(self.prefix)
+        system(cmd)
+
+    def rename_files(self):
+        """ Add the self.dictionary_name prefix to each file generated during the tesseract training process """
+        for f in ['unicharset', 'pffmtable', 'Microfeat', 'inttemp', 'normproto']:
+            cmd = 'mv %s %s.%s' %(f, self.dictionary_name, f)
+            system(cmd)
+
+    def dictionary_data(self):
+        """ Generate dictionaries, coded as a Directed Acyclic Word Graph (DAWG), 
+        from the list of frequent words if those were submitted during the Trainer initialization. 
+        """
+        if self.word_list:
+            freq = 'wordlist2dawg %s %s.freq-dawg %s.unicharset' %(self.word_list, self.dictionary_name, self.dictionary_name)
+            system(freq)
+
+    def combine_data(self):
+        cmd = 'combine_tessdata %s.' %(self.dictionary_name)
+        system(cmd)
+
+    def training(self):
+        """ Execute all training steps """
+        self.generate_boxfile()
+        self.train_on_boxfile()
+        self.compute_character_set()
+        self.clustering()
+        self.normalize()
+        self.rename_files()
+        self.dictionary_data()
+        self.combine_data()
+        print 'The %s.traineddata file has been generated !' %(self.dictionary_name)
+
+    def clean(self):
+        """ Remove all files generated during tesseract training process """
+        print 'cleaning...'
+        cmd = 'rm '
+        cmd += '%s.tr ' %(self.prefix)
+        cmd += '%s.txt ' %(self.prefix)
+        cmd += '%s.box ' %(self.prefix)
+        cmd += '%s.inttemp ' %(self.dictionary_name) 
+        cmd += '%s.Microfeat ' %(self.dictionary_name)
+        cmd += '%s.normproto ' %(self.dictionary_name)
+        cmd += '%s.pffmtable ' %(self.dictionary_name)
+        cmd += '%s.unicharset ' %(self.dictionary_name)
+        if self.word_list:
+            cmd += '%s.freq-dawg ' %(self.dictionary_name)
+        cmd += 'mfunicharset '
+        system(cmd)
+    
+    def add_trained_data(self):
+        """ Copy the newly trained data to the tessdata/ directory """
+        traineddata = '%s.traineddata' %(self.dictionary_name)
+        print 'Copying %s to %s. We need your password for that.' %(traineddata, self.tessdata_path)
+        cmd = 'sudo cp %s %s' %(traineddata, self.tessdata_path)
+        system(cmd)
