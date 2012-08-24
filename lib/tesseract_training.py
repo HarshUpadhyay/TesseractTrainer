dbb9efe2c8bdd54bb68bee97e5053a684c7063b7
Balthazar Rouberol <rouberol.b@gmail.com>
2012-08-24 13:33:45 +0200

    New version: v0.0.3
    Improvements:
     * the tif file size has been reduced by 66% changing from RGB mode to greyscale
     * the location of the `tiffcp` binary has been moved to `defaults.py` and is now stored
       as an environment variable
    
    Bugfixes:
     * The training commands now run if the trainer is initialized with `verbose=False`
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- lib/tesseract_training.py
+++ lib/tesseract_training.py
@@ -4,7 +4,7 @@ of training Tesseract 3described in the Tesseract Wiki:
 https://code.google.com/p/tesseract-ocr/wiki/TrainingTesseract3
 """
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 import shutil
 import os
