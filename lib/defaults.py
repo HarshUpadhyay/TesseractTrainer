fc21e50aa66e6bb3147e87c582db69797fecf861
Balthazar Rouberol <rouberol.b@gmail.com>
2012-08-30 08:46:23 +0200

    Useless import Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- lib/defaults.py
+++ lib/defaults.py
@@ -1,5 +1,3 @@
-import os
-
 FONT_SIZE = 25  # Default font size, used during tif generation
 EXP_NUMBER = 0  # Default experience number, used in generated files name
 FONT_PROPERTIES = './font_properties'  # Default path to the 'font_properties' file
