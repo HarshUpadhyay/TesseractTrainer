7bbe59c5369fa739f70670062ae5a1be4a056a11
Balthazar Rouberol <rouberol.b@gmail.com>
2012-12-25 19:26:19 +0100

    Better file naming
    "lib" is a ridiculous name for a directory. "__main__.py"
    is quite bad as well as it is not used as Python developers
    intended __main__.py files to be used.
    
    Hence, the "main" code is put in the an "if __name__ == '__main__'"
    block, which simplifies the file layout.
    
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- lib/defaults.py
+++
@@ -1,6 +0,0 @@
-FONT_SIZE = 25  # Default font size, used during tif generation
-EXP_NUMBER = 0  # Default experience number, used in generated files name
-FONT_PROPERTIES = './font_properties'  # Default path to the 'font_properties' file
-TESSDATA_PATH = '/usr/local/share/tessdata'  # Default path to the 'tessdata' directory
-WORD_LIST = None  # Default path to the "word_list" file, contaning frequent words
-VERBOSE = True  # verbosity enabled by default. Set to False to remove all text outputs
